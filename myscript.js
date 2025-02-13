07a2202e3016158eb8877838b382b417eeb5c269
Nithika05 <bobbilinithika3@gmail.com>
2025-02-13 22:28:43 +0530

    change

---
+++ myscript.js
@@ -0,0 +1,9 @@
+
+function fun_sign() {
+    window.location.href = "index.html";
+    alert("Sign Up Successful")
+}
+function fun_log() {
+    window.location.href = "index.html";
+    alert("Login Successful")
+}
